524d34cb8f31cfc687fd678556d4306d985a8f9c
MismoMate <165439467+MismoMate@users.noreply.github.com>
2025-06-08 22:46:58 -0500

    Merged parts and course into one object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,44 +1,58 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      { 
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  
 
   return (
     <div>
       <Header course={course} />
-      <Content p1= {part1} p2= {part2} p3= {part3} 
-               exe1= {exercises1} exe2={exercises2} exe3={exercises3} />
-      <Total exe={exercises1 + exercises2 + exercises3} />
+      
+      <Content course={course} />
+      <Total course={course} />
     </div>
   )
 }
 
 const Header = (props) => {
+  console.log(props)
   return (
     <>
-      <h1>{props.course}</h1>
+      <h1>{props.course.name}</h1>
     </>
   )
 }
 
 const Content = (props) => {
+  
   return (
     <>
-      <Part p={props.p1} exec={props.exe1} />
-      <Part p={props.p2} exec={props.exe2} />
-      <Part p={props.p3} exec={props.exe3} />    
+      <Part p={props.course.parts[0].name} exec={props.course.parts[0].exercises} />
+      <Part p={props.course.parts[1].name} exec={props.course.parts[1].exercises} />
+      <Part p={props.course.parts[2].name} exec={props.course.parts[2].exercises} />
     </>
   )
 }
 
-const Total = (props) => {
+const Total = (props) => { 
+  
   return (
     <>
-      <p>Number of exercises {props.exe}</p>
+      <p>Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
     </>
   )
 }
