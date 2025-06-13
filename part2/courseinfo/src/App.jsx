493b201b58e753d4d2381f9b6efa4816b2c30da5
MismoMate <165439467+MismoMate@users.noreply.github.com>
2025-06-13 08:11:25 -0500

    Edited app to accept multiple courses through the map function

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -35,30 +35,58 @@ const Part = (props) => (
 const Total = (props) => <p>Number of exercises {props.total}</p>
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },      
-    ],
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <div>
-      <Course name={course.name} parts={course.parts} />
+      {
+        courses.map(course => 
+          <Course key={course.id} name={course.name} parts={course.parts} />
+        )
+      }      
     </div>
   )
 }
