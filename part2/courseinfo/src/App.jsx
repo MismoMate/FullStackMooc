5b839f002e4a7d3383ddb849e5b3c828930fa593
MismoMate <165439467+MismoMate@users.noreply.github.com>
2025-06-13 08:18:06 -0500

    Refactored Course component and subcomp to their own module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,38 +1,4 @@
-const Course = ({name, parts}) => {
-
-  return (
-    <div>
-      <Header course={name} />
-      <Content parts={parts} />
-      <Total
-        total={
-          parts.reduce((total, part) => total += part.exercises, 0)
-        }
-      />
-    </div>
-  )
-}
-
-const Header = (props) => <h1>{props.course}</h1>
-
-const Content = ({parts}) => (
-  <div>
-    {
-      parts.map(
-        part => 
-        <Part key={part.id} part={part} />
-      )
-    }    
-  </div>
-)
-
-const Part = (props) => (
-  <p>
-    {props.part.name} {props.part.exercises}
-  </p>
-)
-
-const Total = (props) => <p>Number of exercises {props.total}</p>
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
